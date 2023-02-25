8a808b8f10cb633bbb88b9d3e6b3ca1596027cf8
Peter-Jan Gootzen <peter-jan@gootzen.net>
2023-02-25 20:52:25 +0100

    perf runs are now further split up to prevent multiplexing

--- experiments/run.sh
+++ experiments/run.sh
@@ -36,7 +36,7 @@ sudo sh -c "echo -1 > /proc/sys/kernel/perf_event_paranoid"
 
 TIME=$(python3 -c '
 import datetime
-t = 70 + 2*10*8*70 + 2*5*70 + 610*15
+t = 70 + 2*10*8*70 + 2*5*70 + 610*20
 print(datetime.timedelta(seconds=t))
 ')
 echo "The output will be stored under $OUT"
@@ -92,25 +92,29 @@ echo "Reset CPU/DMA latency to default value"
 RUNTIME="600"
 REPS=5
 
+# We are doing -a (system wide profiling) to take the RX path into account that partially doesn't get attributed to the process.
+# Delay the perf measuring by 10s because of the fio startup and warmup time
 echo "Running: perf CPU cycle analysis for ${RUNTIME}s seconds without a load (baseline)"
 for i in $(seq 1 $REPS); do
-	# We are doing -a (system wide profiling) to take the RX path into account that partially doesn't get attributed to the process.
-	# add 10 seconds to the runtime to account for the startup
 	perf stat -a -x "," --delay 10000 --all-kernel -- sleep 610 2> $OUT/cpu_baseline_perf_${i}.out
 done
 
 $PERF_T1_METRICS="cpu-clock,cycles,instructions,branches,branch-misses"
 echo "Running: perf CPU cycle analysis T1 for ${RUNTIME}s seconds with a fio stress load. Measuring the following metrics: ${PERF_T1_METRICS}"
 for i in $(seq 1 $REPS); do
-	# Our fio by default warms up for 10s, so just runtime=300s
 	perf stat -a -x "," --delay 10000 --all-kernel -e $PERF_T1_METRICS -- env RW=randrw BS=4k IODEPTH=128 P=1 RUNTIME=${RUNTIME}s ./workloads/fio.sh 1> $OUT/cpu_load_T1_fio_${i}.out 2> $OUT/cpu_load_T1_perf_${i}.out
 done
 
-$PERF_T2_METRICS="L1-dcache-load-misses,L1-dcache-loads,L1-icache-load-misses,L1-icache-loads,dTLB-loads,dTLB-loads-misses"
-echo "Running: perf CPU cycle analysis T1 for ${RUNTIME}s seconds with a fio stress load. Measuring the following metrics: ${PERF_T2_METRICS}"
+$PERF_T2_METRICS="L1-dcache-load-misses,L1-dcache-loads"
+echo "Running: perf CPU cycle analysis T2 for ${RUNTIME}s seconds with a fio stress load. Measuring the following metrics: ${PERF_T2_METRICS}"
 for i in $(seq 1 $REPS); do
-	# Our fio by default warms up for 10s, so just runtime=300s
 	perf stat -a -x "," --delay 10000 --all-kernel -e $PERF_T2_METRICS -- env RW=randrw BS=4k IODEPTH=128 P=1 RUNTIME=${RUNTIME}s ./workloads/fio.sh 1> $OUT/cpu_load_T2_fio_${i}.out 2> $OUT/cpu_load_T2_perf_${i}.out
+done
+
+$PERF_T3_METRICS="dTLB-loads,dTLB-loads-misses"
+echo "Running: perf CPU cycle analysis T3 for ${RUNTIME}s seconds with a fio stress load. Measuring the following metrics: ${PERF_T3_METRICS}"
+for i in $(seq 1 $REPS); do
+	perf stat -a -x "," --delay 10000 --all-kernel -e $PERF_T3_METRICS -- env RW=randrw BS=4k IODEPTH=128 P=1 RUNTIME=${RUNTIME}s ./workloads/fio.sh 1> $OUT/cpu_load_T3_fio_${i}.out 2> $OUT/cpu_load_T3_perf_${i}.out
 done
 
 echo "DONE"
