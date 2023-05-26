2a6d08d7431e2127800122694405b9b28d623202
Peter-Jan Gootzen <peter-jan@gootzen.net>
2023-05-26 12:13:56 +0000

    Fixed nfs run script (systor submission version)

--- dpfs_nfs/run_max_perf_nfs.sh
+++ dpfs_nfs/run_max_perf_nfs.sh
@@ -9,11 +9,12 @@ export XLIO_SELECT_POLL=100000000
 export XLIO_RX_POLL=-1
 export XLIO_RX_CQ_DRAIN_RATE_NSEC=100
 # We pin XLIO to core 5
-export XLIO_INTERNAL_THREAD_AFFINITY=5
+export XLIO_INTERNAL_THREAD_AFFINITY=4
+export XLIO_MEM_ALLOC_TYPE=0
 
 # XLIO needs tons of hugepage memory, the number 1750 was determined through trial and error to be the lower limit, if the number is too low the read perform will crash HARD
 # Because we cannot use >64 queue depth anyway, lets just give it a ton of memory
-echo 6000 > /sys/kernel/mm/hugepages/hugepages-2048kB/nr_hugepages
+echo 0 > /sys/kernel/mm/hugepages/hugepages-2048kB/nr_hugepages
 # By default there are two threads: a Virtio polling and NFS sending thread + a NFS polling thread
 # We pin them to core 6 and 7
-LD_PRELOAD=/usr/lib/libxlio.so numactl -C 6,7 ./virtionfs -p 0 -v -1 -e mlx5_0 -s 10.100.0.1 -x "/mnt/shared" -d 64
+LD_PRELOAD=/usr/lib/libxlio.so ./dpfs_nfs -c $1
