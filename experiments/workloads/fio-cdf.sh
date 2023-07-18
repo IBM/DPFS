4250a477a82e3a65ad1cb236d2cbc7ad873ff2ea
Peter-Jan Gootzen <peter-jan@gootzen.net>
2023-07-18 18:54:35 +0200

    fixed cdf script
    Signed-off-by: Peter-Jan Gootzen <peter-jan@gootzen.net>

--- experiments/workloads/fio-cdf.sh
+++ experiments/workloads/fio-cdf.sh
@@ -9,7 +9,7 @@ LOG_OUT="${OUT:-"./"}"
 
 FIO_DEFAULTS="--ramp_time=10s --ioengine=io_uring --direct=1 --thread=1 \
   --group_reporting --time_based --random_generator=tausworthe64 --norandommap=1 \
-  --lat_percentiles=1 --percentile_list=25.0:50.0:75.0:99.0 --write_lat_log=\"$LOG_OUT/fio_${RW}_${BS}_${QD}_${P}\""
+  --lat_percentiles=1 --percentile_list=25.0:50.0:75.0:99.0 --write_lat_log=$LOG_OUT/fio_${RW}_${BS}_${QD}_${P}"
 
 fio $FIO_DEFAULTS $FIO_CUSTOM_OPTIONS --filename ${MNT}/fio-$SIZE --size=$SIZE --runtime=$RUNTIME --bs=$BS --rw=$RW \
   --iodepth=$QD --numjobs=$P --name="${RW}_${BS}_${QD}_${P}"
