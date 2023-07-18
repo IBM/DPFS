0f6aa0029d149a7779fd3ce5f31893dc3e7e9592
Peter-Jan Gootzen <peter-jan@gootzen.net>
2023-07-18 19:59:59 +0200

    synthetic fixes
    Signed-off-by: Peter-Jan Gootzen <peter-jan@gootzen.net>

--- experiments/synthetic.sh
+++ experiments/synthetic.sh
@@ -7,22 +7,23 @@ echo "Running synthetic experiments"
 
 TIME=niks
 if [ -n "$MP" ]; then
-	TIME=$(python3 -c '
-	import datetime
-	t = 2*3*8*70 + 2*3*70
-	print(datetime.timedelta(seconds=t))
-	')
+TIME=$(python3 -c '
+import datetime
+t = 2*3*8*5*70 + 2*3*70
+print(datetime.timedelta(seconds=t))
+')
+echo Running Multi-Processor experiments as well
 else
-	TIME=$(python3 -c '
-	import datetime
-	t = 2*3*8*5*70 + 2*3*70
-	print(datetime.timedelta(seconds=t))
-	')
+TIME=$(python3 -c '
+import datetime
+t = 2*3*8*1*70 + 2*3*70
+print(datetime.timedelta(seconds=t))
+')
 fi
 echo "Running: synthetic fio experiments which will take $TIME"
 
 BS_LIST=("4k" "16k" "64k")
-QD_LIST=("2" "4" "8" "16" "32" "64" "128" "256")
+QD_LIST=("2" "4" "8" "16" "32" "64" "128")
 P_LIST=("1")
 if [ -n "$MP" ]; then
 	P_LIST=("1" "2" "4" "8" "10")
@@ -33,7 +34,7 @@ for RW in "randread" "randwrite"; do
 	for BS in "${BS_LIST[@]}"; do
 		for QD in "${QD_LIST[@]}"; do
 			for P in "${P_LIST[@]}"; do
-				echo fio RW=$RW BS=$BS IODEPTH=$IODEPTH P=$P
+				echo fio RW=$RW BS=$BS IODEPTH=$QD P=$P
 				sudo env OUT=$OUT MNT=$MNT BS=$BS QD=$QD P=$P RW=$RW ./workloads/fio.sh > $OUT/fio_${RW}_${BS}_${QD}_${P}.out
 			done
 		done
@@ -47,7 +48,7 @@ for RW in "randread" "randwrite"; do
 	for BS in "${BS_LIST[@]}"; do
 		for QD in 1; do
 			for P in 1; do
-				echo fio RW=$RW BS=$BS IODEPTH=$IODEPTH P=$P
+				echo fio RW=$RW BS=$BS IODEPTH=$QD P=$P
 				sudo env OUT=$OUT MNT=$MNT BS=$BS QD=$QD P=$P RW=$RW ./workloads/fio-cdf.sh > $OUT/fio_${RW}_${BS}_${QD}_${P}.out
 			done
 		done
