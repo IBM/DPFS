fab49bbb514fcca827eee877580cb54d75594074
Peter-Jan Gootzen <peter-jan@gootzen.net>
2023-07-22 12:33:43 +0200

    Improved networking scripts
    Signed-off-by: Peter-Jan Gootzen <peter-jan@gootzen.net>

--- experiments/scripts/dpu_networking.sh
+++ experiments/scripts/dpu_networking.sh
@@ -5,3 +5,6 @@ sudo ip addr add dev enp3s0f0s0 10.100.0.115/24
 sudo ip link set dev enp3s0f0s0 mtu 9000
 sudo ip link set dev p0 mtu 9000
 sudo ip link set dev pf0hpf mtu 9000
+
+echo set up a correct DNS server in /etc/netplan/50-cloud-init.yaml
+echo and then execute sudo netplan apply
