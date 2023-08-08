97339cc1ffc11024524a9569479640f6948601d7
Peter-Jan Gootzen <peter-jan@gootzen.net>
2023-08-08 19:53:22 +0200

    Improved multi-tenancy experiments folder structure
    Signed-off-by: Peter-Jan Gootzen <peter-jan@gootzen.net>

--- experiments/runners/host_ceph_mt.sh
+++ experiments/runners/host_ceph_mt.sh
@@ -1,27 +1,26 @@
 #!/bin/bash
 
-BASE_MNT=$MNT/
-BASE_OUT=$OUT/MT_$MT/
+BASE_MNT=$MNT
+BASE_OUT=$OUT
 
 BS_LIST=("4k" "16k" "64k")
 QD_LIST=("2" "4" "8" "16" "32" "64" "128")
 P_LIST=("1")
-if [ -n "$MP" ]; then
-	P_LIST=("1" "2" "4" "8")
-fi
 
 MT_LIST=("2" "4" "8" "10")
 for MT in "${MT_LIST[@]}"; do
 	echo MT=$MT
+	export OUT=$BASE_OUT/MT_$MT/
 	mkdir -p $OUT
 
 	# Mount
 	for T in $(seq 1 $MT); do
-		MNT=$BASE_MNT\_$T
+		export MNT=$BASE_MNT\_$T
 		sudo mkdir -p $MNT > /dev/null
 		sudo umount -A $MNT > /dev/null
 		sudo mount -t nfs -o wsize=1048576,rsize=1048576,async 10.100.0.19:/pj $MNT
-		echo mounted $MNT
+		sudo -E RUNTIME="10s" RW=randrw BS=4k QD=128 P=4 ./workloads/fio.sh > /dev/null
+		echo mounted and warmed up $MNT
 	done
 
 	# Synthetic
@@ -30,14 +29,12 @@ for MT in "${MT_LIST[@]}"; do
 			for QD in "${QD_LIST[@]}"; do
 				for P in "${P_LIST[@]}"; do
 
+					echo fio RW=$RW BS=$BS QD=$QD P=$P
 					for T in $(seq 1 $MT); do
 						export MNT=$BASE_MNT\_$T
-						export OUT=$BASE_OUT\_$T
-						mkdir -p $OUT
 
-						echo fio RW=$RW BS=$BS QD=$QD P=$P
 						sudo -E env BS=$BS QD=$QD P=$P RW=$RW \
-							./workloads/fio.sh > $OUT/fio_${RW}_${BS}_${QD}_${P}.out &
+							./workloads/fio.sh > $OUT/fio_${RW}_${BS}_${QD}_${P}_${T}.out &
 					done
 					wait
 
@@ -54,12 +51,12 @@ for MT in "${MT_LIST[@]}"; do
 			for QD in 1; do
 				for P in "${P_LIST[@]}"; do
 
+					echo fio RW=$RW BS=$BS QD=$QD P=$P
 					for T in $(seq 1 $MT); do
 						export MNT=$BASE_MNT\_$T
 						export OUT=$BASE_OUT\_$T
 						mkdir -p $OUT
 
-						echo fio RW=$RW BS=$BS QD=$QD P=$P
 						sudo -E env BS=$BS QD=$QD P=$P RW=$RW \
 							./workloads/fio.sh > $OUT/fio_${RW}_${BS}_${QD}_${P}.out &
 					done
